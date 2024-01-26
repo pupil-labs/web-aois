9079ad63ab3a8748fab9293b0f66349f721e4b61
Dominic Canare <dom@dominiccanare.com>
2024-01-26 08:44:03 -0600

    logs requisite events and takes screenshots

---
+++ src/aoi-client.js
@@ -0,0 +1,117 @@
+function zeroPad(num, size) {
+	num = num.toString();
+	while (num.length < size) num = "0" + num;
+	return num;
+}
+
+function createMarkerElement(id, size, brightness){
+	let baseUrl = 'https://raw.githubusercontent.com/AprilRobotics/apriltag-imgs/master/tag36h11/';
+
+	let image = document.createElement('img');
+	image.src = baseUrl + 'tag36_11_' + zeroPad(id, 5) + '.png'
+	image.style.width = size + 'px';
+	image.style.height = size + 'px';
+	image.style.zIndex = 999;
+	image.style.imageRendering = 'pixelated';
+	image.style.filter = "brightness(" + (brightness*100) + "%)";
+	image.style.position = 'fixed';
+
+	return image;
+}
+
+function embedTags(markerSize, brightness, target=null){
+	if(!target){
+		target = document.body;
+	}
+
+	let markers = [
+		createMarkerElement(0, markerSize, brightness),
+		createMarkerElement(1, markerSize, brightness),
+		createMarkerElement(2, markerSize, brightness),
+		createMarkerElement(3, markerSize, brightness),
+	];
+
+	markers[0].style.top = '0px';
+	markers[0].style.left = '0px';
+
+	markers[1].style.top = '0px';
+	markers[1].style.right = '0px';
+
+	markers[2].style.bottom = '0px';
+	markers[2].style.left = '0px';
+
+	markers[3].style.bottom = '0px';
+	markers[3].style.right = '0px';
+
+	let tagContainer = document.createElement('div');
+	tagContainer.id = "apriltag-marker-root-container";
+	for(let marker of markers){
+		marker.className = "apriltag-marker"
+		tagContainer.appendChild(marker);
+	}
+
+	target.appendChild(tagContainer);
+}
+
+function hideTags(){
+	document.getElementById("apriltag-marker-root-container").style.visibility = 'hidden';
+}
+
+function showTags(){
+	document.getElementById("apriltag-marker-root-container").style.visibility = 'visible';
+}
+
+function shimLocationEventsForSPAs(){
+	// https://stackoverflow.com/a/52809105
+	let oldPushState = history.pushState;
+	history.pushState = function pushState() {
+		let ret = oldPushState.apply(this, arguments);
+		window.dispatchEvent(new Event('pushstate'));
+		window.dispatchEvent(new Event('locationchange'));
+		return ret;
+	};
+
+	let oldReplaceState = history.replaceState;
+	history.replaceState = function replaceState() {
+		let ret = oldReplaceState.apply(this, arguments);
+		window.dispatchEvent(new Event('replacestate'));
+		window.dispatchEvent(new Event('locationchange'));
+		return ret;
+	};
+
+	window.addEventListener('popstate', () => {
+		window.dispatchEvent(new Event('locationchange'));
+	});
+}
+
+function installEventListeners(){
+	shimLocationEventsForSPAs();
+
+	window.addEventListener("scroll", (e) => {
+		propagateScrollEvent(window.scrollX, window.scrollY);
+	});
+
+	window.addEventListener("resize", (e) => {
+		propagateResizeEvent(window.innerWidth, window.innerHeight);
+		propagateAOIs();
+	});
+
+	window.addEventListener("focus", (e) => {
+		propagateFocusEvent(window.scrollX, window.scrollY);
+	});
+
+	let last_sent_url = window.location.toString();
+	window.addEventListener('locationchange', (e) => {
+		let url = window.location.toString();
+		if(url != last_sent_url){
+			propagateLocationChangeEvent();
+			propagateAOIs();
+			last_sent_url = url;
+		}
+	});
+
+	propagateLocationChangeEvent();
+	propagateResizeEvent(window.innerWidth, window.innerHeight);
+	propagateAOIs();
+}
+
---
+++ src/main.py
@@ -0,0 +1,214 @@
+import asyncio
+import time
+from pathlib import Path
+
+from playwright.async_api import async_playwright
+from pupil_labs.realtime_api import Device, Network
+
+aoi_type_func_map = {
+    'alt_text': 'get_by_alt_text',
+    'label': 'get_by_label',
+    'placeholder': 'get_by_placeholder',
+    'role': 'get_by_role',
+    'test_id': 'get_by_test_id',
+    'text': 'get_by_text',
+    'title': 'get_by_title',
+}
+
+class BrowserRelay:
+    def __init__(self, pw, device, aois):
+        self.pw = pw
+        self.device = device
+        self.aois = aois
+
+        self.browser = None
+        self.context = None
+
+        self.last_size = (-1, -1)
+        self.tab_info = {}
+
+        self.marker_size = 250
+        self.marker_brightness = 0.5
+
+        self.recording_id = ''
+        self.screenshot_aois_every_load = False
+        self.taking_screen_shots = False
+
+
+    async def playwright_init(self):
+        self.browser = await self.pw.chromium.launch(headless=False, args=['--start-maximized'])
+        self.context = await self.browser.new_context(
+            no_viewport=True,
+            record_video_dir=f"data/{self.recording_id}/"
+        )
+
+        await self.context.add_init_script(path="src/aoi-client.js")
+        await self.context.expose_binding('propagateScrollEvent', self.on_scroll)
+        await self.context.expose_binding('propagateResizeEvent', self.on_resized)
+        await self.context.expose_binding('propagateFocusEvent', self.on_tab_switched)
+        await self.context.expose_binding('propagateLocationChangeEvent', self.on_tab_location_changed)
+        await self.context.expose_binding('propagateAOIs', self.on_aois_changed)
+
+        await self.send_event(f"marker_size_padded={self.marker_size}")
+        self.context.on("page", self.on_new_page)
+
+    async def on_scroll(self, source, x, y):
+        t_ns = time.time_ns()
+        await self.send_scroll(source['page'], x, y, t_ns)
+
+    async def on_resized(self, source, width, height):
+        if self.last_size == (width, height):
+            return
+
+        self.last_size = (width, height)
+        await self.send_event(
+            f"browser_size={width},{height}",
+             event_timestamp_unix_ns=time.time_ns()
+        )
+
+    async def on_new_page(self, page):
+        self.tab_info[page] = {
+            'id': len(self.tab_info),
+            'load_count': -1,
+        }
+        page.on('domcontentloaded', self.on_page_loaded)
+
+    async def on_page_loaded(self, page):
+        if page.url in ['about:blank', 'chrome://newtab/']:
+            return
+
+        await page.evaluate(f'embedTags({self.marker_size}, {self.marker_brightness})')
+        await page.evaluate('installEventListeners()')
+
+    async def on_tab_switched(self, source, scroll_x, scroll_y):
+        t_ns = time.time_ns()
+        tab_id = self.tab_info[source['page']]['id']
+
+        await self.send_event(
+            f"browser_tab={tab_id}", event_timestamp_unix_ns=t_ns
+        )
+        await self.send_scroll(source['page'], scroll_x, scroll_y, t_ns)
+
+    async def on_tab_location_changed(self, source):
+        await self.on_new_url(source['page'])
+
+    async def on_aois_changed(self, source):
+        await self.send_aois(source['page'])
+
+    async def send_scroll(self, page, x, y, t_ns):
+        tab_info = self.tab_info[page]
+        await self.send_event(
+            f"browser_scroll[{tab_info['id']},{tab_info['load_count']}]={x},{y}", event_timestamp_unix_ns=time.time_ns()
+        )
+
+    async def on_new_url(self, page):
+        tab_info = self.tab_info[page]
+        tab_info['load_count'] += 1
+
+        await self.send_event(
+            f"browser_url[{tab_info['id']},{tab_info['load_count']}]={page.url}",
+            event_timestamp_unix_ns=time.time_ns()
+        )
+
+        await page.evaluate("hideTags()")
+        tab_load_id = f"{tab_info['id']}-{tab_info['load_count']}"
+        screenshot_path = Path(f"data/{self.recording_id}/screenshots/{tab_load_id}/")
+        await page.screenshot(path=screenshot_path/"full.png", full_page=True)
+
+        if self.screenshot_aois_every_load:
+            await self.screenshot_aois(page, screenshot_path/'aois')
+
+        await page.evaluate("showTags()")
+
+    async def send_aois(self, page):
+
+        tab_info = self.tab_info[page]
+        tab_load_id = f"[{tab_info['id']},{tab_info['load_count']}]"
+
+        aoi_locators = self.get_aoi_locators_for_page(page)
+        for aoi_name,locator in aoi_locators.items():
+            bounds = await locator.bounding_box()
+            bounds = [bounds['x'], bounds['y'], bounds['width'], bounds['height']]
+            bounds_str = ','.join([str(v) for v in bounds])
+
+            await self.send_event(f"aoi{tab_load_id}[{aoi_name}]={bounds_str}")
+
+    async def send_event(self, event, event_timestamp_unix_ns=None):
+        if self.taking_screen_shots:
+            return
+
+        print('SEND', event)
+        await self.device.send_event(event, event_timestamp_unix_ns=time.time_ns())
+
+    async def record_page(self, url):
+        self.recording_id = await self.device.recording_start()
+
+        if self.browser is None:
+            await self.playwright_init()
+
+        page = await self.context.new_page()
+
+        await page.goto(url)
+
+        while len(self.context.pages) > 0:
+            async with self.context.pages[0].expect_event("close", timeout=0) as _:
+                pass
+
+        await self.device.recording_stop_and_save()
+        await self.context.close()
+
+    async def screenshot_aois(self, page, path=None):
+        self.taking_screen_shots = True
+
+        aoi_locators = self.get_aoi_locators_for_page(page)
+        for aoi_name,locator in aoi_locators.items():
+            await locator.screenshot(path=path/f"{aoi_name}.png")
+
+        await page.evaluate('window.scrollTo(0, 0)')
+        self.taking_screen_shots = False
+
+
+    def get_aoi_locators_for_page(self, page):
+        locators = {}
+        for url,aois in self.aois.items():
+            if page.url == url:
+                for aoi_name,aoi in aois.items():
+                    if aoi['type'] not in aoi_type_func_map:
+                        print('Unrecognized aoi type', aoi['type'])
+                        continue
+
+                    locater_method = getattr(page, aoi_type_func_map[aoi['type']])
+                    locators[aoi_name] = locater_method(**aoi['args'])
+
+        return locators
+
+
+
+
+
+async def main():
+
+    async with Network() as network:
+        dev_info = await network.wait_for_new_device(timeout_seconds=5)
+
+    async with Device.from_discovered_device(dev_info) as device:
+        print('Starting recording!')
+
+        async with async_playwright() as playwright:
+            relay = BrowserRelay(playwright, device, aois={
+                "https://docs.pupil-labs.com/neon/data-collection/data-streams/": {
+                    "gaze": {'type':"role",'args':{'role': "img", 'name':"Gaze"}},
+                    "fixations": {'type':"role",'args':{'role': "img", 'name':"Fixations"}},
+                    "eye_states": {'type':"role",'args':{'role': "img", 'name':"Coordinate systems of 3D eye"}},
+                    "imu_coordinates": {'type':"role",'args':{'role': "img", 'name':"IMU Coordinate System"}},
+                    "imu_camera": {'type':"role",'args':{'role': "img", 'name':"IMU Scene Camera"}},
+                    "imu_orientation": {'type':"role",'args':{'role': "img", 'name':"IMU Pitch, Yaw, Roll"}},
+                },
+            })
+
+            await relay.record_page(
+                url="https://docs.pupil-labs.com/neon/data-collection/data-streams/",
+            )
+
+
+asyncio.run(main())
