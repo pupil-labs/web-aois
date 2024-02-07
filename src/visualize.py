fb1527600879bf2d7818072d0d0aaaa414149540
Dominic Canare <dom@dominiccanare.com>
2024-02-07 00:50:00 -0600

    Saves aoi heatmaps

--- src/visualize.py
+++ src/visualize.py
@@ -13,58 +13,80 @@ class HeatmapVisualizer:
     def __init__(self, recording_path):
         self.recording_path = Path(recording_path)
 
-    def process_full_image(self, scale=0.25, detail=0.01):
-        data_path = self.recording_path.parent / 'data' / 'webpage-aois'
-        for tab_gazes_csv in data_path.glob('*/gazes.csv'):
-            tab_folder = tab_gazes_csv.parent
-            tab_id = tab_folder.name.split('-')[-1]
+    def save_full_heatmap(self, scale=0.25, detail=0.005):
+        self._process_image(
+            self.recording_path.parent / 'data' / 'webpage-aois' / 'tab-0' / 'gazes.csv',
+            self.recording_path.parent / 'screenshots' / '0-0' / 'full.png',
+            scale, detail
+        )
 
-            tab_image = cv2.imread(str(self.recording_path.parent / 'screenshots' / f'{tab_id}-0' / 'full.png'))
-            heatmap = np.zeros(tab_image.shape[:2])
+    def save_aoi_heatmaps(self, scale=0.25, detail=0.025):
+        for aoi_screenshot in (self.recording_path.parent / 'screenshots').glob('*/aois/*.png'):
+            self.save_aoi_heatmap(aoi_screenshot.stem, scale, detail)
 
-            data = np.genfromtxt(tab_gazes_csv, delimiter=',', names=True)
+    def save_aoi_heatmap(self, aoi_name, scale=0.25, detail=0.025):
+        self._process_image(
+            self.recording_path.parent / 'data' / 'webpage-aois' / 'tab-0' / f'aoi-{aoi_name}.csv',
+            self.recording_path.parent / 'screenshots' / '0-0' / 'aois' / f'{aoi_name}.png',
+            scale, detail
+        )
 
-            hist_dims = (
-                int(tab_image.shape[0]*scale),
-                int(tab_image.shape[1]*scale),
-            )
-            gaze_on_surf_x = data['page_x_px'] / (hist_dims[1] / scale)
-            gaze_on_surf_y = data['page_y_px'] / (hist_dims[0] / scale)
+    def _process_image(self, gaze_data_path, screenshot_path, scale=0.25, detail=0.01):
 
-            # make the histogram
-            hist, _, _ = np.histogram2d(
-                gaze_on_surf_y,
-                gaze_on_surf_x,
-                range=[[0, 1.0], [0, 1.0]],
-                bins=hist_dims
-            )
+        screenshot = cv2.imread(str(screenshot_path))
+        heatmap = np.zeros(screenshot.shape[:2])
 
-            # apply gaussian blur
-            filter_h = int(detail * hist_dims[0]) // 2 * 2 + 1
-            filter_w = int(detail * hist_dims[1]) // 2 * 2 + 1
-            heatmap = gaussian_filter(hist, sigma=(filter_w, filter_h), order=0)
+        data = np.genfromtxt(gaze_data_path, delimiter=',', names=True)
 
-            # normalize
-            heatmap /= np.max(heatmap)
+        hist_dims = (
+            int(screenshot.shape[0]*scale),
+            int(screenshot.shape[1]*scale),
+        )
 
-            # scale to image size
-            heatmap = cv2.resize(heatmap, (tab_image.shape[1], tab_image.shape[0]))
+        if 'page_x_px' in data.dtype.names:
+            xy_keys = ('page_x_px', 'page_y_px')
+        else:
+            xy_keys = ('x_px', 'y_px')
 
-            # apply heatmap colors
-            cmap_func = plt.get_cmap('jet')
-            heatmap_image = (cmap_func(heatmap) * 255).astype(np.uint8)
+        gaze_on_surf_x = data[xy_keys[0]] / (hist_dims[1] / scale)
+        gaze_on_surf_y = data[xy_keys[1]] / (hist_dims[0] / scale)
 
-            # add alpha channel
-            heatmap_image[:,:,3] = heatmap * 255
+        # make the histogram
+        hist, _, _ = np.histogram2d(
+            gaze_on_surf_y,
+            gaze_on_surf_x,
+            range=[[0, 1.0], [0, 1.0]],
+            bins=hist_dims
+        )
 
-            # write images
-            cv2.imwrite(str(tab_folder / 'heatmap-full-transparent.png'), heatmap_image)
+        # apply gaussian blur
+        heatmap = gaussian_filter(hist, sigma=15, order=0)
 
-            screenshot = cv2.imread(str(self.recording_path.parent / 'screenshots' / f'{tab_id}-0' / 'full.png'))
-            overlaid = add_overlay(screenshot, heatmap_image)
-            cv2.imwrite(str(tab_folder / 'heatmap-full-overlaid.png'), overlaid)
+        # normalize
+        heatmap /= np.max(heatmap)
+
+        # scale to image size
+        heatmap = cv2.resize(heatmap, (screenshot.shape[1], screenshot.shape[0]))
+
+        # apply heatmap colors
+        cmap_func = plt.get_cmap('jet')
+        heatmap_image = (cmap_func(heatmap) * 255).astype(np.uint8)
+
+        # add alpha channel
+        heatmap_image[:,:,3] = heatmap * 255
+
+        # write images
+        destination = str(gaze_data_path.parent / f'heatmap-{gaze_data_path.stem}-transparent.png')
+        cv2.imwrite(destination, heatmap_image)
+        print('Saved', destination)
+
+        overlaid = add_overlay(screenshot, heatmap_image)
+        destination = str(gaze_data_path.parent / f'heatmap-{gaze_data_path.stem}-overlaid.png')
+        cv2.imwrite(destination, overlaid)
+        print('Saved', destination)
 
 if __name__ == '__main__':
     import sys
     visualizer = HeatmapVisualizer(sys.argv[1])
-    visualizer.process_full_image()
+    visualizer.save_full_heatmap(scale=1.0)
+    visualizer.save_aoi_heatmaps(scale=1.0)
