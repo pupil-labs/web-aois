01842bbb0fada3eb9e8e6ad384ce73a1f0142677
Dominic Canare <dom@dominiccanare.com>
2024-02-07 06:40:36 -0600

    Separate screenshotting

---
+++ src/screenshots.py
@@ -0,0 +1,29 @@
+import sys
+import json
+import asyncio
+from pathlib import Path
+
+from playwright.async_api import async_playwright
+
+from aoi_locator_helper import get_aoi_locators_for_page
+
+async def main():
+    async with async_playwright() as playwright:
+        with open(sys.argv[1], "rt") as aoi_definitions_file:
+            aoi_definitions = json.load(aoi_definitions_file)
+
+        output_path = Path(sys.argv[2])
+
+        browser = await playwright.chromium.launch(headless=False, args=['--start-maximized'])
+        context = await browser.new_context(no_viewport=True)
+
+        page = await context.new_page()
+        for url,aoi_definitions in aoi_definitions.items():
+            await page.goto(url)
+            await page.screenshot(path=output_path / "full-page.png", full_page=True)
+
+            aoi_locators = get_aoi_locators_for_page(page, aoi_definitions)
+            for aoi_name,locator in aoi_locators.items():
+                await locator.screenshot(path=output_path / f"aoi-{aoi_name}.png")
+
+asyncio.run(main())
